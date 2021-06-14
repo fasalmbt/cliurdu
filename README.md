f7cbd003fcf51522f334bb631683988995e19495
fasalmbt <>
2021-06-14 18:13:10 +0530

    Added LICENSE

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 An urdu dictionary utility on cli.
 
 `This repository is made for the purpose of personal use`
+
+### Installation
+
+```
+python3 setup.py install
+```
--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
-
 def readfile(filename):
     with open(filename, 'r+') as f:
         return f.read()
-
 
 setup(
     name="cliurdu",
     version="1.0.0",
     description="A simple cli based utility for urdu dictionary",
     long_description=readfile('README.md'),
+    license=readfile('LICENSE'),
     url="https://github.com/fasalmbt/cliurdu",
     py_modules=['cliurdu'],
     entry_points={
